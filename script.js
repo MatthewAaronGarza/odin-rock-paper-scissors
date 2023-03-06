39269905c6208f000a87ce8eecebc03dc39ba946
Matthew Garza <59824561+MatthewAaronGarza@users.noreply.github.com>
2023-03-06 11:36:35 -0600

    Create pseudo code for algorithm

---
+++ script.js
@@ -0,0 +1,13 @@
+/* 
+
+we're going to have the user enter an input of rock, paper, or scissors
+    need to think of cases where they do not enter any of those
+        let them know and try again
+
+
+the computer will select rock paper or scissors randomly using a function called getComputerChoice
+
+the function playGame(playerSelection, getComputerChoice) will see who between the player and the computer is the winner and print the 
+the results to the player.
+
+*/
