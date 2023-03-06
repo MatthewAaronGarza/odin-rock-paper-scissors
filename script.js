736856f525bc6035a62cc71406ae7082f9dfd4db
Matthew Garza <59824561+MatthewAaronGarza@users.noreply.github.com>
2023-03-06 12:04:10 -0600

    Add playerSelection to pseudo code

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ we're going to have the user enter an input of rock, paper, or scissors
         let them know and try again
 
 
+playerSelection = prompt('Let's play Rock Paper Scissors, please enter your selection!')
+is how we can capture the user input
+
 the computer will select rock paper or scissors randomly using a function called getComputerChoice
 
 the function playGame(playerSelection, getComputerChoice) will see who between the player and the computer is the winner and print the 
